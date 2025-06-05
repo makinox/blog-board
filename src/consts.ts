efd153ae96c62d9566ef2a951e92c112a04400ed
Jesus Bossa <x@jesusbossa.dev>
2025-06-05 18:12:37 -0500

    feat: clean scripts

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Jesús Bossa",
+  NAME: "Voib",
   EMAIL: "x@jesusbossa.dev",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
