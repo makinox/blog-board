32b3f2c14772800daf5c4d0aae2d48049cbd35fd
Jesus Bossa <x@jesusbossa.dev>
2025-06-06 15:41:08 -0500

    feat: improve colors

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,7 @@ import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "Voib",
+  AUTHOR: "Jesús Bossa",
   EMAIL: "x@jesusbossa.dev",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
