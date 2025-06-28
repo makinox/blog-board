ad827ee0bc9dde57e265827b8aa1f28c567ce207
Jesus Bossa <x@jesusbossa.dev>
2025-06-28 18:54:20 -0500

    feat: add tag view

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -38,3 +38,5 @@ export function dateRange(startDate: Date, endDate?: Date | string): string {
 
   return `${startMonth}${startYear} - ${endMonth}${endYear}`;
 }
+
+export const formatTag = (tag: string) => tag.toLowerCase().replace(/ /g, "-");
