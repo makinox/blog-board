b96b82c2505a13e1baf4f247f5c1af9f5cb29221
Jesus Bossa <x@jesusbossa.dev>
2025-07-14 17:35:35 -0500

    feat: add auth pages

---
+++ src/styles/sharedClasses.ts
@@ -0,0 +1,4 @@
+export const sharedClasses = {
+  inputLabel: "label-text text-black/70 dark:text-white/70",
+  input: "input input-bordered w-full bg-white/80 dark:bg-stone-800/80 border-stone-300 dark:border-stone-600 focus:border-stone-500 dark:focus:border-stone-400"
+};
